c4ebd6c6c0d47a0b370ea70e4efe7d22c05b77b0
NathanDalbert <dalbert.nathan@gmail.com>
2024-09-18 22:07:05 -0300

    chore(setup): criar pacotes iniciais para organização da arquitetura

---
+++ src/main/java/com/br/Agro_Mapping/AgroMappingApplication.java
@@ -0,0 +1,13 @@
+package com.br.Agro_Mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroMappingApplication.class, args);
+	}
+
+}
