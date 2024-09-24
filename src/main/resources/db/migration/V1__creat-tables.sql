287e387f1d35cae4a61c6ae7856b9230c7125f6e
NathanDalbert <dalbert.nathan@gmail.com>
2024-09-23 22:21:13 -0300

    feat(database): criar tabelas de produto, usuário, pedido, item_pedido, feira e contato

--- src/main/resources/db/migration/V1__creat-tables.sql
+++ src/main/resources/db/migration/V1__creat-tables.sql
@@ -19,3 +19,24 @@ CREATE TABLE usuario (
     senha VARCHAR(255) NOT NULL,
     data_de_nascimento DATE NOT NULL
 );
+CREATE TABLE pedido (
+    id_pedido UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    data_pedido DATE NOT NULL,
+    valor_total NUMERIC(10, 2) NOT NULL
+);
+CREATE TABLE item_pedido (
+    id_item_pedido UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    preco_unitario NUMERIC(10, 2) NOT NULL,
+    quantidade INTEGER NOT NULL
+);[
+CREATE TABLE feira (
+    id_feira UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    nome VARCHAR(255) NOT NULL,
+    localizacao VARCHAR(255),
+    data_funcionamento DATE NOT NULL
+);
+CREATE TABLE contato (
+    id_contato UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    telefone VARCHAR(255) NOT NULL
+);
+
