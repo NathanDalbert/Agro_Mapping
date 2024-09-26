8fd73f4314eb51a7d8c18a28b6bb2d5be444673f
NathanDalbert <dalbert.nathan@gmail.com>
2024-09-26 08:14:16 -0300

    feat(pedido): criar PedidoMapper

---
+++ src/main/java/com/br/Agro_Mapping/mapper/PedidoMapper.java
@@ -0,0 +1,23 @@
+package com.br.Agro_Mapping.mapper;
+
+import com.br.Agro_Mapping.dto.request.PedidoRequestDTO;
+import com.br.Agro_Mapping.dto.responses.PedidoResponseDTO;
+import com.br.Agro_Mapping.model.Pedido;
+
+public class PedidoMapper {
+    public Pedido toPedido(PedidoRequestDTO pedidoRequestDTO) {
+        return Pedido.newPedido(
+                pedidoRequestDTO.dataPedido(),
+                pedidoRequestDTO.valorTotal());
+
+
+    }
+
+    public PedidoResponseDTO toPedidoResponseDTO(Pedido pedido) {
+        return new PedidoResponseDTO(
+                pedido.getIdPedido(),
+                pedido.getDataPedido(),
+                pedido.getValorTotal()
+        );
+    }
+}
