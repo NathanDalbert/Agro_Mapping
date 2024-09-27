70178f734f7b02a086cf6a25cb26195c873a5c29
NathanDalbert <dalbert.nathan@gmail.com>
2024-09-27 15:54:02 -0300

    fix(Contato): resolver bug de ContatoMapper e ContatoRepository

--- src/main/java/com/br/Agro_Mapping/repository/ContatoRepository.java
+++ src/main/java/com/br/Agro_Mapping/repository/ContatoRepository.java
@@ -2,8 +2,9 @@ package com.br.Agro_Mapping.repository;
 
 import com.br.Agro_Mapping.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface ContatoRepository extends JpaRepository<Contato, UUID> {
 }
--- src/main/java/com/br/Agro_Mapping/service/mapper/ContatoServiceMapper.java
+++ src/main/java/com/br/Agro_Mapping/service/mapper/ContatoServiceMapper.java
@@ -3,7 +3,9 @@ package com.br.Agro_Mapping.service.mapper;
 import com.br.Agro_Mapping.dto.request.ContatoRequestDTO;
 import com.br.Agro_Mapping.dto.responses.ContatoResponseDTO;
 import com.br.Agro_Mapping.model.Contato;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ContatoServiceMapper {
 
     public Contato toContato(ContatoRequestDTO contatoRequestDTO){
