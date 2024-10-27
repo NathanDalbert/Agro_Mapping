e613ca9253abf612b19f8faff804bc904642643f
NathanDalbert <dalbert.nathan@gmail.com>
2024-10-27 09:44:17 -0300

    feat(security): implementar serviço de autorização para carregar usuário por email

---
+++ src/main/java/com/br/Agro_Mapping/service/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.br.Agro_Mapping.service;
+
+import com.br.Agro_Mapping.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
