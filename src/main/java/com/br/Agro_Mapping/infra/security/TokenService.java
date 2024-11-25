ea5b8829395f954a25b2ccc04fa59b5e69e8df29
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-25 11:02:29 -0300

    refactor(TokenService): Ajustar TokenService para retornar UserRole

--- src/main/java/com/br/Agro_Mapping/infra/security/TokenService.java
+++ src/main/java/com/br/Agro_Mapping/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.br.Agro_Mapping.model.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,76 +20,67 @@ public class TokenService {
     @Value("${agro-mapping.token.secret}")
     private String secret;
 
-
     public String generateToken(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
+            return JWT.create()
                     .withIssuer("Agro Mapping")
                     .withSubject(usuario.getEmail())
-                    .withClaim("idUsuario", usuario.getIdUsuario().toString()) // Adiciona o ID do usuário como claim
-                    .withClaim("role", usuario.getUserRole().ordinal()) // Adiciona a role do usuário como claim
+                    .withClaim("idUsuario", usuario.getIdUsuario().toString()) // ID do usuário
+                    .withClaim("role", usuario.getUserRole().name()) // Nome da role no token
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
-            return token;
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token", exception);
         }
     }
 
-    // Método para validar o token e extrair o email
     public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String email = JWT.require(algorithm)
+            DecodedJWT decodedJWT = JWT.require(algorithm)
                     .withIssuer("Agro Mapping")
                     .build()
-                    .verify(token)
-                    .getSubject();
-
-            return email;
+                    .verify(token);
+            return decodedJWT.getSubject(); // Retorna o email do usuário
         } catch (JWTVerificationException e) {
             System.out.println("Erro ao validar o token: " + e.getMessage());
-            return "";
+            return null;
         }
     }
 
-    // Método para extrair o ID do usuário a partir do token
     public UUID extractUserId(String token) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            String idUsuario = JWT.require(algorithm)
-                    .withIssuer("Agro Mapping")
-                    .build()
-                    .verify(token)
-                    .getClaim("idUsuario")
-                    .asString(); // Extrai o ID como String
+            DecodedJWT decodedJWT = decodeToken(token);
+            String idUsuario = decodedJWT.getClaim("idUsuario").asString();
             return UUID.fromString(idUsuario); // Converte para UUID
-        } catch (JWTVerificationException e) {
-            System.out.println("Erro ao extrair o ID do usuário: " + e.getMessage());
-            throw new RuntimeException("Token inválido ou expirado.");
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("ID do usuário inválido no token.", e);
         }
     }
 
-    // Método para extrair a role do usuário a partir do token
     public String extractUserRole(String token) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            String role = JWT.require(algorithm)
-                    .withIssuer("Agro Mapping")
-                    .build()
-                    .verify(token)
-                    .getClaim("role")
-                    .asString(); // Extrai a role como String
-            return role; // Retorna a role
-        } catch (JWTVerificationException e) {
-            System.out.println("Erro ao extrair a role do usuário: " + e.getMessage());
-            throw new RuntimeException("Token inválido ou expirado.");
+            DecodedJWT decodedJWT = decodeToken(token);
+            return decodedJWT.getClaim("role").asString(); // Extrai a role
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao extrair a role do token.", e);
         }
     }
 
-    // Método para gerar a data de expiração do token
-    private Instant genExpirationDate(){
+    private DecodedJWT decodeToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("Agro Mapping")
+                    .build()
+                    .verify(token);
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token inválido ou expirado.", e);
+        }
+    }
+
+    private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
